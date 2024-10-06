89ccef5f8e97a18a281f1e1a7740807f4533d24b
karthik-main <karthik_main@proton.me>
2024-10-06 22:37:48 +0530

    Kyu 8/3.. Opposite number.js

--- Kyu 8/3.. Opposite number.js
+++ Kyu 8/3.. Opposite number.js
@@ -12,11 +12,11 @@ Examples:
 
 
 function opposite(number) {
- if (number = -number){
+ if (number = -number){		// if negative then it returns positve 
    return number
- } else if (number = +number){
+ } else if (number = +number){	// if positive, returns negative
    return -number
  } else {
-   return 0
+   return 0			// returns 0 if number is 0
  }
 }
