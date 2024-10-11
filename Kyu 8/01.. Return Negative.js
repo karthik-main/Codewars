90d0b1020304e5ab27d95bfd0fc82a532d635cc8
karthik-main <karthik_main@proton.me>
2024-10-11 14:08:51 +0530

    Update 01.. Return Negative.js

--- Kyu 8/01.. Return Negative.js
+++ Kyu 8/01.. Return Negative.js
@@ -13,10 +13,11 @@ Zero (0) is not checked for any specific sign. Negative zeros make no mathematic
 
 
 // ------- Solution ---------
+```javacript
 function makeNegative(num) {
   if (num > 0)          // if number is less than 0 then it multiplies it with -1
     return num * -1
   else                  // If number is 0 or more than 0 then it return the number as it is
     return num
-
 }
+```
